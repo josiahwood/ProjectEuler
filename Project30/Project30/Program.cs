d68be14be19fdec902e4927267e1e4033234a7ec
jnwood <jnwood@gmail.com>
2015-10-06 13:08:41 -0500

    Problem30

---
+++ Project30/Project30/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project30
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long powerSum = 0;
+
+			for (int i = 10; i < 9999999; i++)
+			{
+				string s = i.ToString();
+
+				long sum = 0;
+
+				for (int j = 0; j < s.Length; j++)
+				{
+					int digit = int.Parse(s[j].ToString());
+					sum += digit * digit * digit * digit * digit;
+				}
+
+				if (sum == i)
+				{
+					powerSum += sum;
+					Console.WriteLine(sum);
+				}
+			}
+
+			Console.WriteLine("Sum: {0}", powerSum);
+			Console.Read();
+		}
+	}
+}
