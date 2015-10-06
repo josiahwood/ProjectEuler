25555e9f8df438fdad6199c7049e0b71478815f4
jnwood <jnwood@gmail.com>
2015-10-06 09:34:38 -0500

    Problem20

---
+++ Problem20/Problem20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem20
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BigInteger f = 1;
+
+			for (ulong i = 2; i <= 100; i++)
+			{
+				f *= i;
+			}
+
+			string str = f.ToString();
+			long sum = 0;
+
+			for (int i = 0; i < str.Length; i++)
+			{
+				sum += long.Parse(str[i].ToString());
+			}
+
+			Console.WriteLine(sum);
+			Console.Read();
+		}
+	}
+}
