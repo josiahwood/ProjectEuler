72ba9ed7e4c38b9d613b989a7a9b467be21be225
jnwood <jnwood@gmail.com>
2015-10-08 18:30:57 -0500

    Problem69

---
+++ Problem69/Problem69/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem69
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 2 * 3 * 5 * 7 * 11 * 13 * 17 = 510510
+		}
+	}
+}
