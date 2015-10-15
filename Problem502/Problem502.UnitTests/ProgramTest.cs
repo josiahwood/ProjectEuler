196c1cf62dd3958721c852cc8b7b9428a60c72b3
jnwood <jnwood@gmail.com>
2015-10-15 17:48:11 -0500

    Simplified algorithm

--- Problem502/Problem502.UnitTests/ProgramTest.cs
+++ Problem502/Problem502.UnitTests/ProgramTest.cs
@@ -8,95 +8,136 @@ namespace Problem502.UnitTests
 	public class ProgramTest
 	{
 		[TestMethod]
-		public void WithBase1_1()
+		public void Solve1_1()
 		{
-			Result result = Program.WithBase(1, 1);
-			Assert.AreEqual<ulong>(0, result.MaxHeightEven);
-			Assert.AreEqual<ulong>(1, result.MaxHeightOdd);
-			Assert.AreEqual<ulong>(0, result.NotMaxHeightEven);
-			Assert.AreEqual<ulong>(0, result.NotMaxHeightOdd);
+			Result result = Program.Solve(1, 1);
+#if MODULUS
+			Assert.AreEqual<ulong>(0, result.Even);
+			Assert.AreEqual<ulong>(1, result.Odd);
+#else
+			Assert.AreEqual<BigInteger>(0, result.Even);
+			Assert.AreEqual<BigInteger>(1, result.Odd);
+#endif
 		}
 		
 		[TestMethod]
-		public void WithoutBase1_1()
+		public void SubSolve1_1()
 		{
-			Result result = Program.WithoutBase(1, 1);
-			Assert.AreEqual<ulong>(0, result.MaxHeightEven);
-			Assert.AreEqual<ulong>(1, result.MaxHeightOdd);
-			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
-			Assert.AreEqual<ulong>(0, result.NotMaxHeightOdd);
+			Result result = Program.SubSolve(1, 1);
+#if MODULUS
+			Assert.AreEqual<ulong>(1, result.Even);
+			Assert.AreEqual<ulong>(1, result.Odd);
+#else
+			Assert.AreEqual<BigInteger>(1, result.Even);
+			Assert.AreEqual<BigInteger>(1, result.Odd);
+#endif
 		}
 
 		[TestMethod]
-		public void WithBase1_2()
+		public void Solve1_2()
 		{
-			Result result = Program.WithBase(1, 2);
+			Result result = Program.Solve(1, 2);
+#if MODULUS
 			Assert.AreEqual<ulong>(1, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(0, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(1, result.Even);
+			Assert.AreEqual<BigInteger>(1, result.Odd);
+#endif
 		}
 		
 		[TestMethod]
-		public void WithoutBase1_2()
+		public void SubSolve1_2()
 		{
-			Result result = Program.WithoutBase(1, 2);
+			Result result = Program.SubSolve(1, 2);
+#if MODULUS
 			Assert.AreEqual<ulong>(1, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(2, result.Even);
+			Assert.AreEqual<BigInteger>(1, result.Odd);
+#endif
 		}
 
 		[TestMethod]
-		public void WithoutBase1_3()
+		public void SubSolve1_3()
 		{
-			Result result = Program.WithoutBase(1, 3);
+			Result result = Program.SubSolve(1, 3);
+#if MODULUS
 			Assert.AreEqual<ulong>(0, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(1, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(2, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(2, result.Even);
+			Assert.AreEqual<BigInteger>(2, result.Odd);
+#endif
 		}
 
 		[TestMethod]
-		public void WithoutBase2_1()
+		public void SubSolve2_1()
 		{
-			Result result = Program.WithoutBase(2, 1);
+			Result result = Program.SubSolve(2, 1);
+#if MODULUS
 			Assert.AreEqual<ulong>(0, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(3, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(1, result.Even);
+			Assert.AreEqual<BigInteger>(3, result.Odd);
+#endif
 		}
 
 		[TestMethod]
-		public void WithoutBase2_2()
+		public void SubSolve2_2()
 		{
-			Result result = Program.WithoutBase(2, 2);
+			Result result = Program.SubSolve(2, 2);
+#if MODULUS
 			Assert.AreEqual<ulong>(5, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(3, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(6, result.Even);
+			Assert.AreEqual<BigInteger>(3, result.Odd);
+#endif
 		}
 		
 		[TestMethod]
-		public void WithoutBase4_1()
+		public void SubSolve4_1()
 		{
-			Result result = Program.WithoutBase(4, 1);
+			Result result = Program.SubSolve(4, 1);
+#if MODULUS
 			Assert.AreEqual<ulong>(5, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(10, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(6, result.Even);
+			Assert.AreEqual<BigInteger>(10, result.Odd);
+#endif
 		}
 		
 		[TestMethod]
-		public void WithBase4_2()
+		public void Solve4_2()
 		{
-			Result result = Program.WithBase(4, 2);
+			Result result = Program.Solve(4, 2);
+#if MODULUS
 			Assert.AreEqual<ulong>(10, result.MaxHeightEven);
 			Assert.AreEqual<ulong>(5, result.MaxHeightOdd);
 			Assert.AreEqual<ulong>(0, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightOdd);
+#else
+			Assert.AreEqual<BigInteger>(10, result.Even);
+			Assert.AreEqual<BigInteger>(6, result.Odd);
+#endif
 		}
-
+		/*
 		[TestMethod]
 		public void WithoutBase4_2()
 		{
@@ -168,26 +209,26 @@ namespace Problem502.UnitTests
 			Assert.AreEqual<ulong>(1, result.NotMaxHeightEven);
 			Assert.AreEqual<ulong>(0, result.NotMaxHeightOdd);
 		}
-
+		*/
 		[TestMethod]
-		public void WithBase13_10()
+		public void F13_10()
 		{
-			Result result = Program.WithBase(13, 10);
-			Assert.AreEqual<ulong>(3729050610636 % Program.Mod, result.MaxHeightEven);
+			BigInteger result = Program.F(13, 10);
+			Assert.AreEqual<BigInteger>(3729050610636, result);
 		}
 
 		[TestMethod]
-		public void WithBase10_13()
+		public void F10_13()
 		{
-			Result result = Program.WithBase(10, 13);
-			Assert.AreEqual<ulong>(37959702514 % Program.Mod, result.MaxHeightEven);
+			BigInteger result = Program.F(10, 13);
+			Assert.AreEqual<BigInteger>(37959702514, result);
 		}
-
+		
 		[TestMethod]
-		public void WithBase100_100()
+		public void F100_100()
 		{
-			Result result = Program.WithBase(100, 100);
-			Assert.AreEqual<ulong>(841913936, result.MaxHeightEven);
+			BigInteger result = Program.F(100, 100);
+			Assert.AreEqual<BigInteger>(841913936, result % 1000000007);
 		}
 	}
 }
--- Problem502/Problem502/Program.cs
+++ Problem502/Problem502/Program.cs
@@ -32,25 +32,30 @@ namespace Problem502
 	
 	public struct Result
 	{
+		public static readonly Result OneEven = new Result(1, 0);
+		public static readonly Result OneOdd = new Result(0, 1);
+
 #if MODULUS
 		public ulong MaxHeightOdd;
 		public ulong MaxHeightEven;
 		public ulong NotMaxHeightOdd;
 		public ulong NotMaxHeightEven;
 #else
-		public BigInteger MaxHeightOdd;
-		public BigInteger MaxHeightEven;
-		public BigInteger NotMaxHeightOdd;
-		public BigInteger NotMaxHeightEven;
+		public BigInteger Even;
+		public BigInteger Odd;
 #endif
+
+		public Result(BigInteger even, BigInteger odd)
+		{
+			Even = even;
+			Odd = odd;
+		}
 
 		public static Result operator +(Result left, Result right)
 		{
 			Result result;
-			result.MaxHeightEven = Program.Add(left.MaxHeightEven, right.MaxHeightEven);
-			result.MaxHeightOdd = Program.Add(left.MaxHeightOdd, right.MaxHeightOdd);
-			result.NotMaxHeightEven = Program.Add(left.NotMaxHeightEven, right.NotMaxHeightEven);
-			result.NotMaxHeightOdd = Program.Add(left.NotMaxHeightOdd, right.NotMaxHeightOdd);
+			result.Even = Program.Add(left.Even, right.Even);
+			result.Odd = Program.Add(left.Odd, right.Odd);
 			return result;
 		}
 
@@ -58,33 +63,26 @@ namespace Problem502
 		{
 			Result result = new Result();
 
-			result.MaxHeightEven += Program.Multiply(left.MaxHeightEven, right.MaxHeightEven);
-			result.MaxHeightOdd += Program.Multiply(left.MaxHeightEven, right.MaxHeightOdd);
-			result.MaxHeightEven += Program.Multiply(left.MaxHeightEven, right.NotMaxHeightEven);
-			result.MaxHeightOdd += Program.Multiply(left.MaxHeightEven, right.NotMaxHeightOdd);
-
-			result.MaxHeightOdd += Program.Multiply(left.MaxHeightOdd, right.MaxHeightEven);
-			result.MaxHeightEven += Program.Multiply(left.MaxHeightOdd, right.MaxHeightOdd);
-			result.MaxHeightOdd += Program.Multiply(left.MaxHeightOdd, right.NotMaxHeightEven);
-			result.MaxHeightEven += Program.Multiply(left.MaxHeightOdd, right.NotMaxHeightOdd);
-
-			result.MaxHeightEven += Program.Multiply(left.NotMaxHeightEven, right.MaxHeightEven);
-			result.MaxHeightOdd += Program.Multiply(left.NotMaxHeightEven, right.MaxHeightOdd);
-			result.NotMaxHeightEven += Program.Multiply(left.NotMaxHeightEven, right.NotMaxHeightEven);
-			result.NotMaxHeightOdd += Program.Multiply(left.NotMaxHeightEven, right.NotMaxHeightOdd);
-
-			result.MaxHeightOdd += Program.Multiply(left.NotMaxHeightOdd, right.MaxHeightEven);
-			result.MaxHeightEven += Program.Multiply(left.NotMaxHeightOdd, right.MaxHeightOdd);
-			result.NotMaxHeightOdd += Program.Multiply(left.NotMaxHeightOdd, right.NotMaxHeightEven);
-			result.NotMaxHeightEven += Program.Multiply(left.NotMaxHeightOdd, right.NotMaxHeightOdd);
+			result.Even += Program.Multiply(left.Even, right.Even);
+			result.Odd += Program.Multiply(left.Even, right.Odd);
+			result.Odd += Program.Multiply(left.Odd, right.Even);
+			result.Even += Program.Multiply(left.Odd, right.Odd);
 
 			return result;
+		}
+
+		public Result Swap
+		{
+			get
+			{
+				return new Result(Odd, Even);
+			}
 		}
 	}
 
 	public class Program
 	{
-		static SplayTree<Request, Result> _withoutBaseResults = new SplayTree<Request, Result>();
+		static SplayTree<Request, Result> _results = new SplayTree<Request, Result>();
 
 #if MODULUS
 		public const ulong Mod = 1000000007;
@@ -123,215 +121,69 @@ namespace Problem502
 		[STAThread]
 		static void Main(string[] args)
 		{
-			//Result result0 = WithBase(BigInteger.Pow(10, 12), 100);
+			//BigInteger result0 = F(BigInteger.Pow(10, 12), 100);
 			//Console.WriteLine(result0.MaxHeightEven);
 
-			double[] coefficients = Coefficients();
-
-			//StringBuilder s = new StringBuilder();
-
-			//for (int w = 1; w <= 10; w++)
-			//{
-			//	for (int h = 1; h <= 10; h++)
-			//	{
-			//		Result result = WithBase(w, h);
-			//		BigInteger a = 0;
-			//		//a += result.MaxHeightEven;
-			//		//a += result.MaxHeightOdd;
-			//		a += result.NotMaxHeightEven;
-			//		a += result.NotMaxHeightOdd;
-			//		s.Append(a + "\t");
-			//	}
-
-			//	s.AppendLine();
-			//}
-
-			//Clipboard.SetText(s.ToString());
-
-			//Result result1 = WithBase(10000, 10000);
-			//Console.WriteLine(result1.MaxHeightEven);
+			BigInteger result1 = F(10000, 10000);
+			Console.WriteLine(result1);
 			
 			Console.Read();
 		}
 
+#if MODULUS
 		public static ulong F(BigInteger w, BigInteger h)
-		{
-			// Total(w, h) = h ^ w
-			// NotMaxHeight(w, h) = (h - 1) ^ w
-			// MaxHeight(w, h) = Total(w, h) - NotMaxHeight(w, h)
-			// Odd(w, h) = 2 ^ (w - 1) * (h / 2) ^ w - (w - 1) * (h / 2) ^ (w - 1)
-			// Odd(w, h) = (h ^ w) / 2 - (w - 1) * (h / 2) ^ (w - 1)
-			// Odd(w, h) = Total(w, h) / 2 - (w - 1) * (h / 2) ^ (w - 1)
-			// Even(w, h) = Total(w, h) / 2 + (w - 1) * (h / 2) ^ (w - 1)
-			// Even(w, h) = (h ^ w) / 2 + (w - 1) * (h / 2) ^ (w - 1)
-			// NotMaxHeightEven(w, h) = ((h - 1) ^ w) / 2 + (w - 1) * (h / 2) ^ (w - 1)
-
-			return 0;
-		}
-
-		static double[] Coefficients()
-		{
-			const int size = 10;
-			Matrix<double> a = CreateMatrix.Dense<double>(size, size);
-			Matrix<double> b = CreateMatrix.Dense<double>(size, 1);
-			
-			for (int i = 0; i < size; i++)
-			{
-				long x = i + 1;
-				Result result = WithBase(8, x * 2);
-				long y = (long)(result.MaxHeightOdd + result.NotMaxHeightOdd);
-
-				long xe = 1;
-
-				for (int j = 0; j < size; j++)
-				{
-					a[i, j] = xe;
-					xe *= x;
-				}
-
-				b[i, 0] = y;
-			}
-
-			Matrix<double> c = a.Inverse() * b;
-			return c.Column(0).ToArray();
-		}
-
-		//static bool Evaluate(long[] coefficients)
-		//{
-		//	int maxE = (int)Math.Sqrt(coefficients.Length);
-			
-		//	for (int w = 10; w <= 100; w++)
-		//	{
-		//		for (int h = 10; h <= 100; h++)
-		//		{
-		//			Result result = WithBase(w, h);
-		//			ulong expected = result.MaxHeightEven;
-		//			long actual = 0;
-		//			ulong wc = 1;
-
-		//			for (int we = 0; we < maxE; we++)
-		//			{
-		//				ulong hc = 1;
-
-		//				for (int he = 0; he < maxE; he++)
-		//				{
-		//					actual += ModMultiply(coefficients[we * maxE + he], (long)ModMultiply(wc, hc));
-
-		//					hc *= (ulong)h;
-		//				}
-
-		//				wc *= (ulong)w;
-		//			}
-
-		//			if (actual != (long)expected)
-		//			{
-		//				return false;
-		//			}
-		//		}
-		//	}
-
-		//	return true;
-		//}
-
-		public static Result WithBase(BigInteger w, BigInteger h)
-		{
-			Result result;
-			
-			if (w == 0)
-			{
-				result.MaxHeightEven = 0;
-				result.MaxHeightOdd = 0;
-				result.NotMaxHeightEven = 1;
-				result.NotMaxHeightOdd = 0;
-				
-				return result;
-			}
-			else if (h == 1)
-			{
-				result.MaxHeightEven = 0;
-				result.MaxHeightOdd = 1;
-				result.NotMaxHeightEven = 0;
-				result.NotMaxHeightOdd = 0;
-
-				return result;
-			}
-			else if (w == 1)
-			{
-#if MODULUS
-				result.MaxHeightEven = (ulong)(1 - h % 2);
-				result.MaxHeightOdd = (ulong)(h % 2);
-				result.NotMaxHeightEven = (ulong)((((h + 1) >> 1) - 1) % Mod);
-				result.NotMaxHeightOdd = (ulong)((h >> 1) % Mod);
 #else
-				result.MaxHeightEven = 1 - h % 2;
-				result.MaxHeightOdd = h % 2;
-				result.NotMaxHeightEven = ((h + 1) >> 1) - 1;
-				result.NotMaxHeightOdd = h >> 1;
+		public static BigInteger F(BigInteger w, BigInteger h)
 #endif
+		{
+			Result result0 = Solve(w, h);
+			Result result1 = Solve(w, h - 1);
 
-				return result;
-			}
-			
-			Result subResult = WithoutBase(w, h - 1);
-
-			result.MaxHeightEven = subResult.MaxHeightOdd;
-			result.MaxHeightOdd = subResult.MaxHeightEven;
-			result.NotMaxHeightEven = subResult.NotMaxHeightOdd;
-			result.NotMaxHeightOdd = subResult.NotMaxHeightEven;
-
-			return result;
+			return result0.Even - result1.Even;
 		}
 
-		public static Result WithoutBase(BigInteger w, BigInteger h)
+		public static Result Solve(BigInteger w, BigInteger h)
 		{
-			Result result;
-			
-			if (w == 0)
-			{
-				result.MaxHeightEven = 0;
-				result.MaxHeightOdd = 0;
-				result.NotMaxHeightEven = 1;
-				result.NotMaxHeightOdd = 0;
+			return SubSolve(w, h - 1).Swap;
+		}
 
-				return result;
-			}
-			else if (w == 1)
+		public static Result SubSolve(BigInteger w, BigInteger h)
+		{
+			if (w <= 0 || h == 0)
 			{
-#if MODULUS
-				result.MaxHeightEven = (ulong)(1 - h % 2);
-				result.MaxHeightOdd = (ulong)(h % 2);
-				result.NotMaxHeightEven = (ulong)(((h + 1) >> 1) % Mod);
-				result.NotMaxHeightOdd = (ulong)((h >> 1) % Mod);
-#else
-				result.MaxHeightEven = 1 - h % 2;
-				result.MaxHeightOdd = h % 2;
-				result.NotMaxHeightEven = (h + 1) >> 1;
-				result.NotMaxHeightOdd = h >> 1;
-#endif
-
-				return result;
+				return Result.OneEven;
 			}
-			
+
 			Request request;
 			request.Width = w;
 			request.Height = h;
 
-			if (_withoutBaseResults.TryGetValue(request, out result))
+			Result result;
+
+			if (_results.TryGetValue(request, out result))
 			{
 				return result;
 			}
-			
-			result = WithBase(w, h);
 
-			for (BigInteger i = 0; i < w; i++)
+			for (BigInteger i = 0; i <= w; i++)
 			{
-				Result withResult = WithBase(i, h);
-				Result withoutResult = WithoutBase(w - i - 1, h);
+				Result withResult;
+
+				if (i == 0)
+				{
+					withResult = Result.OneEven;
+				}
+				else
+				{
+					withResult = SubSolve(i, h - 1).Swap;
+				}
+
+				Result withoutResult = SubSolve(w - i - 1, h);
 				Result total = withResult * withoutResult;
 				result += total;
 			}
 
-			_withoutBaseResults.Add(request, result);
+			_results.Add(request, result);
 			return result;
 		}
 	}
