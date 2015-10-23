34f9abbd27f8e94d110353d153e5dbd8f057a2e3
jnwood <jnwood@gmail.com>
2015-10-23 18:18:15 -0500

    Problem 66 WIP

---
+++ Problem66/Problem66/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem66
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// 741 is incorrect
+			long result = Solve(1000);
+			Console.WriteLine(result);
+			Console.Read();
+		}
+
+		static long Solve(long maxD)
+		{
+			long maxDSquared = maxD * maxD;
+			
+			List<long> squares = new List<long>();
+
+			for (long s = 1; s <= maxDSquared; s++)
+			{
+				squares.Add(s * s);
+			}
+			
+			long maxX = 0;
+			long bestD = 0;
+
+			for (long d = 2; d <= maxD; d++)
+			{
+				if (!squares.Contains(d))
+				{
+					for (long x = 1; x <= maxDSquared * maxDSquared; x++)
+					{
+						// x^2 - Dy^2 = 1
+						// -Dy^2 = 1 - x^2
+						// y^2 = (x^2 - 1)/D
+
+						if (((x * x) - 1) % d == 0)
+						{
+							long ySquared = ((x * x) - 1) / d;
+
+							if (squares.Contains(ySquared))
+							{
+								if (x >= maxX)
+								{
+									maxX = x;
+									bestD = d;
+								}
+
+								break;
+							}
+						}
+					}
+				}
+			}
+
+			return bestD;
+		}
+	}
+}
