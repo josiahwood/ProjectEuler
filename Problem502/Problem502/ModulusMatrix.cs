dd42b75142a17ac3a7ae777a9fa9c2770f37ecc3
jnwood <jnwood@gmail.com>
2015-10-26 08:50:10 -0500

    Added ModulusNumber and ModulusMatrix

---
+++ Problem502/Problem502/ModulusMatrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem502
+{
+	class ModulusMatrix
+	{
+		private int _rows;
+		private int _columns;
+		private ModulusNumber[,] _values;
+
+		public ModulusMatrix(int rows, int columns)
+		{
+			_rows = rows;
+			_columns = columns;
+			_values = new ModulusNumber[rows, columns];
+		}
+
+		public ModulusNumber this[int row, int column]
+		{
+			get
+			{
+				return _values[row, column];
+			}
+
+			set
+			{
+				_values[row, column] = value;
+			}
+		}
+
+		public ModulusNumber[] Column(int index)
+		{
+			ModulusNumber[] column = new ModulusNumber[_rows];
+
+			for (int i = 0; i < _rows; i++)
+			{
+				column[i] = _values[i, index];
+			}
+
+			return column;
+		}
+
+		public static ModulusMatrix operator *(ModulusMatrix left, ModulusMatrix right)
+		{
+			if (left._columns != right._rows)
+			{
+				throw new Exception("Incompatible matrices");
+			}
+
+			ModulusMatrix result = new ModulusMatrix(left._rows, right._columns);
+
+			for (int i = 0; i < left._rows; i++)
+			{
+				for (int j = 0; j < right._columns; j++)
+				{
+					for (int k = 0; k < left._columns; k++)
+					{
+						result._values[i, j] += left[i, k] * right[k, j];
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Problem502/Problem502/ModulusNumber.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem502
+{
+	struct ModulusNumber
+	{
+		public const ulong Mod = 1000000007;
+		
+		public static readonly ModulusNumber Zero = new ModulusNumber(0);
+		public static readonly ModulusNumber One = new ModulusNumber(1);
+
+		public ulong Value;
+
+		public ModulusNumber(ulong value)
+		{
+			Value = value;
+		}
+
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+
+		public static bool operator ==(ModulusNumber left, ModulusNumber right)
+		{
+			return left.Value == right.Value;
+		}
+
+		public static bool operator !=(ModulusNumber left, ModulusNumber right)
+		{
+			return left.Value != right.Value;
+		}
+
+		public static bool operator >(ModulusNumber left, ModulusNumber right)
+		{
+			return left.Value > right.Value;
+		}
+
+		public static bool operator <(ModulusNumber left, ModulusNumber right)
+		{
+			return left.Value < right.Value;
+		}
+
+		public static ModulusNumber operator +(ModulusNumber left, ModulusNumber right)
+		{
+			ulong value = (left.Value + right.Value) % Mod;
+			return new ModulusNumber(value);
+		}
+
+		public static ModulusNumber operator -(ModulusNumber left, ModulusNumber right)
+		{
+			ulong value = (Mod + left.Value - right.Value) % Mod;
+			return new ModulusNumber(value);
+		}
+
+		public static ModulusNumber operator *(ModulusNumber left, ModulusNumber right)
+		{
+			ulong value = (left.Value * right.Value) % Mod;
+			return new ModulusNumber(value);
+		}
+	}
+}
