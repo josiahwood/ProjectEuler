d0b72e725dbec8206786f4616e7e1852a82fd822
Josiah Wood <jnwood@gmail.com>
2016-05-25 10:23:45 -0500

    Added code for Problem1

---
+++ Problem1/Problem1.UnitTests/Problem1Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problem1.UnitTests
+{
+	[TestClass]
+	public class Problem1Test
+	{
+		[TestMethod]
+		public void StatementExample()
+		{
+			int result = Program.Solve(10);
+			Assert.AreEqual(23, result);
+		}
+	}
+}
---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem1
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int result = Solve(1000);
+			Console.WriteLine(result);
+		}
+
+		public static int Solve(int limit)
+		{
+			return Sum(limit, 3) + Sum(limit, 5) - Sum(limit, 15);
+		}
+
+		static int Sum(int limit, int increment)
+		{
+			int n = (limit - 1) / increment;
+			return increment * n * (n + 1) / 2;
+		}
+	}
+}
